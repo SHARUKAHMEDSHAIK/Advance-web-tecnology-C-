898b728e04c31631745b3cb7ae72d082f29b1006
SHARUKAHMEDSHAIK <87636558+SHARUKAHMEDSHAIK@users.noreply.github.com>
2022-04-30 00:04:42 +0600

    Lab task _6

---
+++ Final/lab task 6/lab task-6/src/Components/StudentData.js
@@ -0,0 +1,39 @@
+const StudentData =[
+    {
+        id: 1,
+        name: "Anindita Roy",
+        studentId: "19-40001-1",
+        batch: "19-1",
+        cgpa: "3.5",
+        address: "sylhet",
+        department: "CSE"
+    },
+    {
+        id: 2,
+        name: "Nuzhut Tabassum",
+        studentId: "19-39987-1",
+        batch: "19-1",
+        cgpa: "3.6",
+        address: "Dhaka",
+        department: "CSE"
+    },
+    {
+        id: 3,
+        name: "Muhaiminul Islam",
+        studentId: "19-40002-1",
+        batch: "19-1",
+        cgpa: "3.4",
+        address: "Manikgonj",
+        department: "CSE"
+    },
+    {
+        id: 4,
+        name: "Oishi Roy",
+        studentId: "19-40034-1",
+        batch: "19-1",
+        cgpa: "3.5",
+        address: "Dhaka",
+        department: "CSE"
+    }
+]
+export default StudentData;
